8e34a35044e4a6c7824d1b590db8cc519d7b013d
usman-qsols <usmanrahim.qsols@gmail.com>
2024-04-14 02:22:25 +0500

    Mint token function UI changed

--- src/componensts/nftDetails.tsx
+++ src/componensts/nftDetails.tsx
@@ -149,7 +149,7 @@ const NftDetailPage = () => {
               </p>
             </div>
             {data?.price !== 0 || data?.ownerAddress === address ? (
-              <button className="flex mt-4 ml-auto text-white bg-sky-400 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600 rounded">
+              <>
                 {data?.ownerAddress === address ? (
                   <button
                     onClick={() => {
@@ -165,7 +165,7 @@ const NftDetailPage = () => {
                     Buy NFT
                   </button>
                 )}
-              </button>
+              </>
             ) : (
               <button
                 disabled={true}
